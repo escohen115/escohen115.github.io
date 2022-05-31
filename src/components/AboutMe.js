638a6219d8e13b402954e9f52080f982f89d93e5
simon <escohen115@gmail.com>
2022-05-30 20:40:06 -0400

    a

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,7 @@ function AboutMe(){
         <h2>A bit about me</h2>
         Hello and welcome!<br></br>
         My name is Simon Cohen. <br></br>
-        I am an Atlanta-based software engineer. Aside from studying data structures and algorithms, I am also learning Java and NodeJS in my free time. Other hobbies of mine include, reading, rollerblading, and <a href="https://www.youtube.com/watch?v=bjDm_tb6Euw&ab_channel=CommodoreKinematics">  building custom Lego automatons.</a>
+        I am an Atlanta-based software engineer. Aside from studying data structures and algorithms, I am also learning Java and NodeJS in my free time. Other hobbies of mine include, reading, rollerblading, and <a href="https://www.youtube.com/watch?v=bjDm_tb6Euw&ab_channel=CommodoreKinematics"> building custom Lego automatons.</a>
 
     </div>)
 }
