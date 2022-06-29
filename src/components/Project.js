f70580c282b7a0ca2310b8a142662b4491e7dc8a
simon <escohen115@gmail.com>
2022-06-29 19:44:34 -0400

    a

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,8 +7,10 @@ function Project({project}) {
     return (<div >
         <h3 className="project-titles">{project.projectName}</h3>
         <div className="projectLinks">
-            <a target="_blank" rel="noreferrer" className="github-links" href={project.gitHubFrontend}>Github Frontend</a>  
-            <a target="_blank" rel="noreferrer" className="github-links" href={project.gitHubBackend}>Github Backend</a>
+            <ul>
+                <a target="_blank" rel="noreferrer" className="github-links" href={project.gitHubFrontend}>Github Frontend </a>  
+                <a target="_blank" rel="noreferrer" className="github-links" href={project.gitHubBackend}>Github Backend</a>
+            </ul>
         </div>
         <YoutubeEmbed embedId={project.demo} />
         <p className="description">{project.description}</p>
