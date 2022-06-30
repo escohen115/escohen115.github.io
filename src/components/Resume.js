3009ac13219fad79424e4fdbded27ca2a94e841c
simon <escohen115@gmail.com>
2022-06-30 19:58:10 -0400

    npm run deply

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,10 @@
 import { Document } from 'react-pdf'
-import {PDF} from "..images/Simon Cohen Resume.pdf"
+// import PDF from "..images/Simon Cohen Resume.pdf"
+
 function Resume(){
     return <div className = "bottomRight"> 
         <h2 style={{textAlign:"left"}}>Resume</h2>
-        <Document file={PDF}/>
+        {/* <Document file={PDF}/> */}
         
     </div>
 }
